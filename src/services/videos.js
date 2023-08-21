05948550f0d9c76db27fc38afd15cf11eb2a9655
RenanLeviathan <renan.leviathan@gmail.com>
2023-08-21 18:39:14 -0300

    Mudanca de repositorio

--- src/services/videos.js
+++ src/services/videos.js
@@ -1,39 +1,11 @@
 const VIDEOS = [
 	{
-		"href" : "https://www.youtube.com/embed/dlXF0-WL_IY",
-		"title" : "Masculinidade frágil de Ross - Leo Hwan"
-	},
-	{
-		"title" : "Masculinidade frágil? Homem não pode chorar?",
-		"href" : "https://www.youtube.com/embed/74LDVaF_-2o"
+		"href" : "https://www.youtube.com/embed/t8tjT9MA7yU",
+		"title" : "Funny Cats and Dogs Compilation"
 	},
 	{
 		"title" : "DJ contacreast playlist",
 		"href" : "https://www.youtube.com/embed/j1dXVf_c2RM"
-	},
-	{
-		"title" : "O que é a masculinidade frágil?",
-		"href" : "https://www.youtube.com/embed/RhRXlC-hHJ4"
-	},
-	{
-		"title" : "TODO MUNDO É BI? BISSEXUALIDADE: MITOS E VERDADES com Raphael Andrade",
-		"href" : "https://youtube.com/embed/BRbulUcOBPg"
-	},
-	{
-		"title" : "Qual foi a última vez que você experimentou algo novo? / Jhanda Siqueira",
-		"href" : "https://www.youtube.com/embed/negQrVAT8ng"
-	},
-	{
-		"title" : "Wolves of the North",
-		"href" : "https://youtube.com/embed/UOD8144ZUg4"
-	},
-	{
-		"title" : "Mario Lopez - Sound Of City [Deepforces Remix]",
-		"href" : "https://www.youtube.com/embed/cgQndH_IB4E"
-	},
-	{
-		"title" : "Gabry Ponte - La danza delle streghe",
-		"href" : "https://www.youtube.com/embed/redAfu1a6ec"
 	}
 ];
 
