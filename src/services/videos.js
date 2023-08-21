c7645ba83f64bf0a19577386fb2f1a450e870fd2
RenanLeviathan <renan.leviathan@gmail.com>
2023-08-21 18:54:53 -0300

    Adição de mais videos

--- src/services/videos.js
+++ src/services/videos.js
@@ -6,6 +6,18 @@ const VIDEOS = [
 	{
 		"title" : "DJ contacreast playlist",
 		"href" : "https://www.youtube.com/embed/j1dXVf_c2RM"
+	},
+    {
+		"title" : "Wolves of the North",
+		"href" : "https://youtube.com/embed/UOD8144ZUg4"
+	},
+	{
+		"title" : "Mario Lopez - Sound Of City [Deepforces Remix]",
+		"href" : "https://www.youtube.com/embed/cgQndH_IB4E"
+	},
+	{
+		"title" : "Gabry Ponte - La danza delle streghe",
+		"href" : "https://www.youtube.com/embed/redAfu1a6ec"
 	}
 ];
 
